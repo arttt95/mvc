30a47926be88ce54547df68fa2bbeb9b4c6c16c7
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-11-01 02:28:28 -0300

    01NOV24 - Aula 531 - MVC (Conexão com banco de dados e Models).

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,27 +3,39 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class IndexController extends Action {
 
     public function index() {
-
+/*
         $this->view->dados = [
             'Sofá',
             'Cadeira',
             'Cama'
-        ];
+        ];*/
+
+        // Instância de conexão
+        $conn = Connection::getDb();
+
+        // Instânciar modelo
+        $produto = new Produto($conn);
+
+        $produtos = $produto->getProdutos();
+
+        $this->view->dados = $produtos;
 
         $this->render('index', 'layout1');
     }
 
     public function sobreNos() {
-
+/*
         $this->view->dados = [
             'Notebook',
             'Smartphone',
             'Video-game'
-        ];
+        ];*/
 
         $this->render('sobreNos', 'layout1');
     }
